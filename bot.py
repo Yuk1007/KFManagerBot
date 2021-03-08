4ba2616419776deffeba3d6e7d660636ee4ea0ba
Yuki Takae <s236146@wakayama-u.ac.jp>
2021-03-08 13:03:57 +0900

    version update

--- bot.py
+++ bot.py
@@ -18,13 +18,34 @@ async def on_ready():
 async def timer():
     now = datetime.now()
     hours = now.strftime('%H:%M')
-    if hours == '11:54':
+    if hours == '00:00':
         channel = client.get_channel(CHANNEL_ID)
-        message = await channel.send('スクリムに出る人は○出ない人は×を押してね！')
-        circle = '\U00002b55'
-        batu = '\U0000274c'
-        await message.add_reaction(circle)
-        await message.add_reaction(batu)
+        embed = discord.Embed(title="本日のスクリム業務", description=":grapes: スクリム申請、18時の連絡\n"
+                                                             ":apple: ロースター発表後のロースター掲載、観戦枠申請、浮上時間の連絡\n"
+                                                             ":cherries: ランドマークリストの作成、貼り出し\n"
+                                                             ":peach: ランドマップ作成\n"
+                                                             ":tangerine: 番号貼り出し、スクリム中の観戦、スクショ・総合結果貼り出し\n"
+                                                             ":kiwi: 戦績入力\n"
+                                                             ":ok: すべて可\n"
+                                                             ":ng: いずれも不可")
+        message = await channel.send(embed=embed)
+        grape = '\U0001f347'
+        apple = '\U0001f34e'
+        cherries = '\U0001f352'
+        peach = '\U0001f351'
+        tangerine = '\U0001f34a'
+        kiwi = '\U0001f95d'
+        ok = '\U0001f197'
+        ng = '\U0001f196'
+        await message.add_reaction(apple)
+        await message.add_reaction(grape)
+        await message.add_reaction(cherries)
+        await message.add_reaction(peach)
+        await message.add_reaction(tangerine)
+        await message.add_reaction(kiwi)
+        await message.add_reaction(ok)
+        await message.add_reaction(ng)
+
 
 @timer.before_loop
 async def before_timer():
