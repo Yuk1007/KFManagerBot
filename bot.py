75ea996184718b1b585778ebd4e399d0c84cdd7c
Yuki Takae <s236146@wakayama-u.ac.jp>
2021-04-18 14:04:24 +0900

    version update

--- bot.py
+++ bot.py
@@ -22,12 +22,13 @@ async def timer():
         channel = client.get_channel(CHANNEL_ID)
         embed = discord.Embed(title="本日のスクリム業務", description=":grapes: スクリム申請、18時の連絡\n"
                                                              ":apple: ロースター発表後のロースター掲載、観戦枠申請、浮上時間の連絡\n"
-                                                             ":cherries: ランドマークリストの作成、貼り出し\n"
                                                              ":peach: ランドマップ作成\n"
-                                                             ":tangerine: 番号貼り出し、スクリム中の観戦、スクショ・総合結果貼り出し\n"
+                                                             ":tangerine: 番号貼り出し、スクショ・総合結果貼り出し\n"
+                                                             ":cherries: スクリム中の観戦\n"
                                                              ":kiwi: 戦績入力\n"
                                                              ":ok: すべて可\n"
-                                                             ":ng: いずれも不可")
+                                                             ":ng: いずれも不可\n"
+                                                             ":question: 未定")
         message = await channel.send(embed=embed)
         grape = '\U0001f347'
         apple = '\U0001f34e'
@@ -37,14 +38,16 @@ async def timer():
         kiwi = '\U0001f95d'
         ok = '\U0001f197'
         ng = '\U0001f196'
+        question = '\U00002754'
         await message.add_reaction(grape)
         await message.add_reaction(apple)
-        await message.add_reaction(cherries)
         await message.add_reaction(peach)
         await message.add_reaction(tangerine)
+        await message.add_reaction(cherries)
         await message.add_reaction(kiwi)
         await message.add_reaction(ok)
         await message.add_reaction(ng)
+        await message.add_reaction(question)
 
 
 @timer.before_loop
