dcbb11edf5be37b139447558914090bd4d703bec
yuki <s236146@wakayama-u.ac.jp>
2021-06-22 21:11:00 +0900

    version update

--- bot.py
+++ bot.py
@@ -20,34 +20,28 @@ async def timer():
     hours = now.strftime('%H')
     if hours == '00':
         channel = client.get_channel(CHANNEL_ID)
-        embed = discord.Embed(title="本日のスクリム業務", description=":grapes: スクリム申請、18時の連絡\n"
-                                                             ":apple: ロースター発表後のロースター掲載、観戦枠申請、浮上時間の連絡\n"
+        embed = discord.Embed(title="本日のスクリム業務", description=":grapes: スクリム申請\n"
+                                                             ":apple: ロースター掲載、浮上時間の連絡\n"
                                                              ":peach: ランドマップ作成\n"
                                                              ":tangerine: 番号貼り出し、スクショ・総合結果貼り出し\n"
                                                              ":cherries: スクリム中の観戦\n"
-                                                             ":kiwi: 戦績入力\n"
-                                                             ":ok: すべて可\n"
-                                                             ":ng: いずれも不可\n"
-                                                             ":question: 未定")
+                                                             ":ok: すべて可"
+                              )
         message = await channel.send(embed=embed)
         grape = '\U0001f347'
         apple = '\U0001f34e'
         cherries = '\U0001f352'
         peach = '\U0001f351'
         tangerine = '\U0001f34a'
-        kiwi = '\U0001f95d'
         ok = '\U0001f197'
-        ng = '\U0001f196'
-        question = '\U00002754'
+
         await message.add_reaction(grape)
         await message.add_reaction(apple)
         await message.add_reaction(peach)
         await message.add_reaction(tangerine)
         await message.add_reaction(cherries)
-        await message.add_reaction(kiwi)
+
         await message.add_reaction(ok)
-        await message.add_reaction(ng)
-        await message.add_reaction(question)
 
 
 @timer.before_loop
